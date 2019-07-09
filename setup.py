0b3507d1e935b5823d71bee9c69eacaf513ae0ce
Fatih Sucu <fatihsucu@Fatihs-MacBook-Pro.local>
2019-07-09 12:56:35 +0300

    updates version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-skew-correction',
-    version='0.1',
+    version='0.2',
     description='Python Text Image Skew Correction',
     url='https://github.com/fatihsucu/python-skew-correction',
     author='Fatih Sucu',
